612a16b8a655f24dc1f2a8e0cb06f7e727714a7f
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-16 15:52:07 +0530

    display position

---
+++ uc1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_leader_problem
+{
+    public class SnakeLeaderCode
+    {
+        public static void displayposition()
+        {
+            Console.WriteLine("Start the Snake and Ladder Game");
+            Console.WriteLine("Single Player at Start Position 0");
+        }
+    }
+}
