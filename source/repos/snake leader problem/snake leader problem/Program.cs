92705e96021c21c2d5756028274df99703a840fc
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-16 17:09:35 +0530

    problem solving snakend leader

---
+++ source/repos/snake leader problem/snake leader problem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace snake_leader_problem
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Leader Game");
+            //SnakeLeaderCode.displayposition();
+            //SnakeLeaderuc2.rollsnddiesnumber();
+            //SnakeLeaderuc3.checkcases();
+            //SnakeLeaderuc4.richtllmax();
+            //SnakeLeaderuc5.exactwinningposition();
+            //SnakeLeaderuc6.call();
+            SnakeLeaderuc7.addplayer2();
+        }
+
+    }
+}
+
