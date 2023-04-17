05bfb71d51b1857c06223aa060442d252f33cfbc
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-17 21:46:25 +0530

    code to giving conditions to password to have atleast special character

---
+++ source/repos/266/UserRegistrationProblem/UserRegistrationProblem/passwordrule4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    public class passwordrule4
+    {
+        public static string PassWord = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=?]).{8,}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, PassWord);
+        }
+    }
+}
