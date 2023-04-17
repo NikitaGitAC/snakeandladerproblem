e34ded2fed128da2941031b71e3187977eb3e50d
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-17 21:44:35 +0530

    user io/op

---
+++ source/repos/266/UserRegistrationProblem/UserRegistrationProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Page");
+            passwordrule4 passWord = new passwordrule4();
+            Console.WriteLine("Please enter password");
+            string password = Console.ReadLine();
+            Console.WriteLine(passWord.ValidatePassword(password));
+        }
+    }
+}
